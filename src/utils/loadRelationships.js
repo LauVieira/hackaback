e033fcc94659bb1257ca35c6a9f24f6b69b84339
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-06 17:49:16 -0300

    Models relationships

---
+++ src/utils/loadRelationships.js
@@ -0,0 +1,12 @@
+const Career = require('../models/Career');
+const CareerUser = require('../models/User');
+const Session = require('../models/Session');
+const User = require('../models/User');
+const UserData = require('../models/UserData');
+
+User.belongsToMany(Career, { through: CareerUser });
+Career.belongsToMany(User, { through: CareerUser });
+
+User.hasMany(UserData);
+User.hasMany(Session);
+Session.belongsTo(User);
