ffd03a2026d48edf7be349bd72fa0d019a49e9cc
hoffmannpaola <hoffmannpaola@gmail.com>
2021-02-07 18:47:07 -0300

    Commit deploy config database

--- src/utils/database.js
+++ src/utils/database.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
